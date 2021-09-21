734606cb1c5a3fcfe659ac71ca502847654f079f
NohemiGonzalez <ngonzalez@1aauto.com>
2021-09-21 08:11:52 -0600

    Add sidenav menu frontend

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -2,10 +2,14 @@ $(document).ready(function(){
     $('#search-link').click(function(){
         $('#search-autocomplete-input').focus();
     });
+
     $('.dropdown-trigger').dropdown();
 
     $('.select-lang').click(function(e){
         const selectedOption = e.target.getAttribute("id");
         window.location.replace('?lang=' + selectedOption);
     });
+
+    $('.sidenav').sidenav();
+
 });
