b962726d20af03b776d3de4353586f144e00c836
NohemiGonzalez <ngonzalez@1aauto.com>
2021-11-16 18:31:05 -0700

    Add extends SpringBootServletInitializer

--- src/main/java/com/tiikopapas/site/SiteApplication.java
+++ src/main/java/com/tiikopapas/site/SiteApplication.java
@@ -4,12 +4,13 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 @Log4j2
 @SpringBootApplication
-public class SiteApplication {
+public class SiteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiteApplication.class, args);
